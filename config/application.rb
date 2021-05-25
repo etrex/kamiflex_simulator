37c3bc2394f55a1e882aa863318cdbc487771679
etrex kuo <et284vu065k3@gmail.com>
2021-05-25 10:47:05 +0800

    fix host

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module KamiflexSimulator
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "ngrok.etrex.tw"
+    config.hosts << "kamiflex-simulator.herokuapp.com"
   end
 end
