c31a9814a97f9b571a9c80b55fc1295fa7efe03a
etrex kuo <et284vu065k3@gmail.com>
2021-07-10 00:50:12 +0800

    update kamiflex & add liff

---
+++ docs/core.js
@@ -0,0 +1,74 @@
+let editor = monaco.editor.create(document.getElementById("editor"), {
+  value: `Kamiflex.hash(self) do
+  alt_text "hello world!"
+  bubble do
+    body do
+      text "hello world!"
+    end
+  end
+end`,
+  language: "ruby"
+});
+
+let jsonViewer = monaco.editor.create(document.getElementById("json_viewer"), {
+  value: `{}`,
+  readOnly: true,
+  language: "ruby"
+})
+
+function updateJsonViewer(){
+  const json = JSON.stringify(flexMessage, null, 2)
+  jsonViewer.setValue(json);
+}
+
+async function getFlexMessage(code){
+  window.json = await callRuby(code);
+  return JSON.parse(json);
+}
+
+async function callRuby(code){
+  const rubyWasmResult = await window.ruby(code);
+  const outputs = rubyWasmResult.output;
+  const jsons = [];
+  let startCollectJson = false;
+  outputs.forEach(function(output){
+    console.log(output.output);
+    if(output.output[0] == "{"){
+      startCollectJson = true;
+    }
+    if(startCollectJson){
+      jsons.push(output.output)
+    }
+  })
+  return jsons.join("");
+}
+
+function updateFlexMessageViewer(){
+  $("#line_flex_message").html("")
+  flex2html("line_flex_message", flexMessage)
+}
+
+let code = editor.getValue()
+let timeout;
+let flexMessage = "{}";
+
+async function update(){
+  console.log("update");
+  flexMessage = await getFlexMessage(code)
+  updateFlexMessageViewer()
+  updateJsonViewer()
+}
+
+document.addEventListener("keyup", function(e){
+  let currentCode = editor.getValue()
+  if(currentCode == code){
+    return;
+  }else{
+    code = currentCode;
+    if(timeout){
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(update, 500);
+  }
+});
+update();
---
+++ docs/liff.js
@@ -0,0 +1,38 @@
+async function liff_init(){
+  console.log("liff_init")
+  await liff.init({
+    liffId: "1656198526-p1wZyEGE"
+  });
+  if (!liff.isLoggedIn()) {
+    liff.login();
+  }
+}
+
+async function share_target_picker(){
+  /* JS 不支援的情況 */
+  if (!liff.isApiAvailable('shareTargetPicker')){
+    return;
+  }
+
+  let messages = JSON.stringify([flexMessage]);
+
+  /* 分享訊息給好友 */
+  const response = await liff.shareTargetPicker(messages);
+
+  /* 分享訊息失敗 */
+  if(!response){
+    const [majorVer, minorVer] = (liff.getLineVersion() || "").split('.');
+    if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
+      alert("你的 LINE 版本太舊，無法使用分享訊息功能。");
+    } else {
+      alert("你已取消分享訊息。");
+    }
+  }
+}
+
+window.onload = async function() {
+  console.log("init")
+  if(typeof(liff) !== 'undefined'){
+    await liff_init();
+  }
+};
